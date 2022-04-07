86092682a2cf1d1e9dd8b5c75b50e825b9f53b20
navarcus <bspalamutcu@gmail.com>
2022-04-07 22:56:48 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -5,16 +5,22 @@ import './App.css';
 import './index.css';
 
 const KEY = '946cb26e574944edb81221535220604';
+const weatherData = [];
+const locationData = [];
 
 function App() {
   const api = axios.create({
     baseURL: 'http://api.weatherapi.com/v1',
   });
   useEffect(() => {
-    api.get(`/current.json?key=${KEY}&q=istanbul`);
+    const fetchData = async () => {
+      const currentData = await api.get(`/current.json?key=${KEY}&q=kusadasi`);
+      console.log(currentData.data);
+    };
+    fetchData().catch((error) => console.log(error));
   }, [api]);
 
-  return <div className="App">BERKE</div>;
+  return <div className="App">hehehe</div>;
 }
 
 export default App;
