cef4faba7774d0890d7404a155b39e0daa1b7fd8
navarcus <bspalamutcu@gmail.com>
2022-04-07 21:43:56 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import './index.css';
+
+const KEY = '946cb26e574944edb81221535220604';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const api = axios.create({
+    baseURL: 'http://api.weatherapi.com/v1',
+  });
+  useEffect(() => {
+    api.get(`/current.json?key=${KEY}&q=istanbul`);
+  }, [api]);
+
+  return <div className="App">BERKE</div>;
 }
 
 export default App;
