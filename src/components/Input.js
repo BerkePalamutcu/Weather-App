8d1305d0d4e36254ced1352b70a8170cd32b5cac
navarcus <bspalamutcu@gmail.com>
2022-04-09 06:04:08 +0300

    textChange detection functionality added

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,16 +3,16 @@ import React, { useState } from 'react';
 function TextInput() {
   const [text, textChange] = useState('');
   const handleText = (e) => {
-    let input = textChange(e.target.value);
+    textChange(e.target.value);
   };
   return (
     <div>
       <form>
         <input
-          name={text}
           onChange={handleText}
           className="locationInput"
           type="text"
+          value={text}
           required
         ></input>
         <button type="submit">search</button>
