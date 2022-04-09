229c4874d7ab8769d16d15c16bc8af996484f243
navarcus <bspalamutcu@gmail.com>
2022-04-09 22:27:27 +0300

    search component was transferred to input.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
 import './App.css';
 import './index.css';
-import TextInput from './components/Input';
-
-const key = process.env.REACT_APP_API_KEY;
+import SearchLocation from './components/Input';
 
 function App() {
-  const [weatherData, setWeatherData] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const { data } = await axios.get(
-        `https://api.weatherapi.com/v1/current.json`,
-        {
-          params: {
-            key: key,
-            q: 'kusadasi',
-            lang: 'tr',
-          },
-        }
-      );
-      console.log(data);
-      console.log(weatherData);
-      setWeatherData(data);
-    };
-    fetchData().catch((error) => console.log(error));
-  }, []);
-
   return (
     <div className="App">
-      <TextInput></TextInput>
+      <SearchLocation></SearchLocation>
     </div>
   );
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function TextInput() {
-  const [text, textChange] = useState('kusadasi');
+const key = process.env.REACT_APP_API_KEY;
+
+function SearchLocation() {
+  const [text, textChange] = useState('');
   const handleText = (e) => {
     textChange(e.target.value);
   };
+  console.log(text);
+  const [weatherData, setWeatherData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get(
+        `https://api.weatherapi.com/v1/current.json`,
+        {
+          params: {
+            key: key,
+            q: text,
+            lang: 'tr',
+          },
+        }
+      );
+
+      console.log(weatherData);
+      setWeatherData(data);
+      console.log(weatherData);
+    };
+    fetchData().catch((error) => console.log(error));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [text]);
   return (
     <div>
       <form>
@@ -19,4 +44,4 @@ function TextInput() {
     </div>
   );
 }
-export default TextInput;
+export default SearchLocation;
