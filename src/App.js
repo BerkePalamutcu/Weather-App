b1857121b5503545180e1c01f03e45e0ad3c3bf1
navarcus <bspalamutcu@gmail.com>
2022-04-13 19:51:13 +0300

    use effect was moved to the main component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import './index.css';
 import SearchLocation from './components/Input';
+import axios from 'axios';
 
 function App() {
+  const key = process.env.REACT_APP_API_KEY;
+  const [text, textChange] = useState('');
+  const [weatherData, setWeatherData] = useState([]);
+
+  const handleText = (e) => {
+    textChange(e.target.value);
+  };
+
+  const fetchData = async () => {
+    const { data } = await axios.get(
+      `https://api.weatherapi.com/v1/current.json`,
+      {
+        params: {
+          key: key,
+          q: text,
+          lang: 'tr',
+        },
+      }
+    );
+    setWeatherData(data);
+  };
+
+  useEffect(() => {
+    if (text != '') {
+      try {
+        fetchData();
+      } catch (error) {
+        console.log(error);
+      }
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }
+  }, [text]);
+  console.log(weatherData);
   return (
     <div className="App">
-      <SearchLocation></SearchLocation>
+      <SearchLocation handleText={handleText} text={text}></SearchLocation>
     </div>
   );
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,39 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-
-const key = process.env.REACT_APP_API_KEY;
-
-function SearchLocation() {
-  const [text, textChange] = useState('');
-  const [weatherData, setWeatherData] = useState([]);
-
-  const handleText = (e) => {
-    textChange(e.target.value);
-  };
-
-  const fetchData = async () => {
-    const { data } = await axios.get(
-      `https://api.weatherapi.com/v1/current.json`,
-      {
-        params: {
-          key: key,
-          q: text,
-          lang: 'tr',
-        },
-      }
-    );
-    setWeatherData(data);
-  };
-
-  useEffect(() => {
-    try {
-      fetchData();
-    } catch (error) {
-      console.log(error);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [text]);
-  console.log(weatherData);
+function SearchLocation({ handleText, text }) {
   return (
     <div>
       <form>
