d5f0b31852ac1fc12c81392a2354fdfa19cdec0f
navarcus <bspalamutcu@gmail.com>
2022-04-14 23:40:48 +0300

    Revert "timeout function was added"
    This reverts commit 50be873046dac533db16b2567790de097f9500e0.

--- src/App.js
+++ src/App.js
@@ -3,29 +3,33 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import './index.css';
 import SearchLocation from './components/Input';
+
 import axios from 'axios';
 
+const key = process.env.REACT_APP_API_KEY;
 function App() {
-  const key = process.env.REACT_APP_API_KEY;
   const [text, textChange] = useState('');
   const [weatherData, setWeatherData] = useState([]);
+  const { location, current } = weatherData;
 
   const handleText = (e) => {
+    e.preventDefault();
     textChange(e.target.value);
   };
-
   const fetchData = async () => {
-    const { data } = await axios.get(
-      `https://api.weatherapi.com/v1/current.json`,
-      {
-        params: {
-          key: key,
-          q: text,
-          lang: 'en',
-        },
-      }
-    );
-    setWeatherData(data);
+    if (text) {
+      const { data } = await axios.get(
+        `https://api.weatherapi.com/v1/current.json`,
+        {
+          params: {
+            key: key,
+            q: text,
+            lang: 'en',
+          },
+        }
+      );
+      setWeatherData(data);
+    }
   };
 
   useEffect(() => {
@@ -37,16 +41,33 @@ function App() {
           console.log(error);
         }
       }
-    }, 2000);
+    }, 5000);
     return () => {
       clearTimeout(timeOut);
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [text]);
-  console.log(weatherData);
+  }, [text, weatherData]);
+  console.log(current);
   return (
     <div className="App">
       <SearchLocation handleText={handleText} text={text}></SearchLocation>
+      <div key={Math.random()} className="weatherCard">
+        <h1>{location ? `Country : ${location.country}` : undefined}</h1>
+        <h1>{location ? `City Name: ${location.name}` : undefined}</h1>
+        <hr></hr>
+        <h1>{current ? `Current: ${current.temp_c} degrees` : undefined}</h1>
+        <h1>
+          {current ? `Feels like: ${current.feelslike_c} degrees` : undefined}
+        </h1>
+        <h1>{current ? `Condition: ${current.condition.text} ` : undefined}</h1>
+        <h1>{current ? `Windspeed: ${current.wind_kph} Kph ` : undefined}</h1>
+        <h1>{current ? `Wind direction: ${current.wind_dir} ` : undefined}</h1>
+        <h1>{current ? `Humidity: %${current.humidity} ` : undefined}</h1>
+        <h1>{current ? `Pressure: ${current.pressure_mb} Mb` : undefined}</h1>
+        <h1>
+          {current ? `Last update: ${current.last_updated} LT` : undefined}
+        </h1>
+      </div>
     </div>
   );
 }
